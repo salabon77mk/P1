6356121554a52d9b941baf9c6e30b5fe6b5ab26f
Mick K <salabon77mk@gmail.com>
2018-09-15 17:59:52 -0500

    Prototype for procexplore

--- procexplore.c
+++ procexplore.c
@@ -1,1 +1,94 @@
 // Proc Explore
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <ctype.h>
+
+
+//First function should return only the PID specified
+
+
+static int isUserPID(long processNum);
+
+
+long int * getPID(){
+	struct dirent *dp;
+	long int userPIDs[1024];
+	long int *PIDs = userPIDs;
+
+	DIR *dir = opendir("/proc");
+
+	while((dp=readdir(dir)) != NULL){
+		int count = 0;
+		if(!isdigit(*dp->d_name)){
+			continue;	}
+
+		long int pidNum = strtol(dp->d_name, NULL, 10);
+		int i = isUserPID(pidNum);
+
+		
+		if( i  == 1){
+			userPIDs[i] = pidNum;		
+		  	count++;
+		}
+		
+
+	}
+	return PIDs;	
+
+}
+
+static int isUserPID(long processNum){
+	char filepath[256];
+	char line[100];
+	FILE *fptr;
+	
+	snprintf(filepath, 256, "/proc/%ld/status", processNum);
+
+	fptr = fopen(filepath, "r");
+
+	if(!fptr){	
+		return 0;
+	}	
+
+
+	while(fgets(line, 100, fptr)){
+		
+		if(strncmp(line, "Uid:", 4) == 0){
+			
+			char minUID[4]; //we only need the first four dig to see if its a UUSER
+			strncpy(minUID, line + 5, 4);
+			long int UID = strtol(minUID, NULL, 10);
+			/*
+			printf("Line: %s", line);
+			printf("String: %s\n", minUID);
+			printf("UID: %ld\n\n", UID);
+			*/
+			if(UID >= 1000){
+				fclose(fptr);
+				return 1;
+			}
+
+			else{
+				fclose(fptr);
+				return 0;
+			}
+		}
+		
+		
+	
+	}
+	
+}
+
+
+int main(){
+	long int * gimme = getPID();
+	
+
+
+	return 0;	
+}
--- statexplore.c
+++ statexplore.c
@@ -1,1 +1,15 @@
 // Stat explore
+// -s, defaults to false, Single char, located in proc/<pid>/stat, 3rd field
+// -U, defaults to true,  User time in seconds?, located in stat, 14th field
+// -S, defaults to false, System time consumed, in stat, 15th field
+// -v, defaults to false, statm, 1st field
+// -c, defaults to true, cmdline file, defaults to true
+
+
+struct statFields {
+	char state;
+	unsigned int utime;
+	unsigned int stime;
+	unsigned int size;
+	char *cmd;
+};
