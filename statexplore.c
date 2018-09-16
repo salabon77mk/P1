789cc41c505c21ec85b159c0e7877a2d7f6c79c2
Mick K <salabon77mk@gmail.com>
2018-09-16 16:48:01 -0500

    Fleshed out skeleton

--- statexplore.c
+++ statexplore.c
@@ -17,27 +17,74 @@ struct pidStats {
 	char *cmd;
 };
 
-static void readStat(long int pids, struct pidStats *pid);
-static void readStatm(long int pids, struct pidStats *pid);
+static void readCmdline(long int pid, struct pidStats *pidStat);
+static void readStat(long int pid, struct pidStats *pidStat);
+static void readStatm(long int pid, struct pidStats *pidStat);
 
 void getpidinfo(long int *pids, struct pidStats **stats, size_t pidSize, size_t statStructCount){
 	for(int i = 0; i < statStructCount / sizeof(stats) ; i++){
 		readStat(pids[i], stats[i]);
 		readStatm(pids[i], stats[i]);
+		readCmdline(pids[i], stats[i]);
 	}
 
 
 
 }
 
-static void readStat(long int pids, struct pidStats *pid){
+static void readStat(long int pid, struct pidStats *pidStat){
+	char filepath[256];
+	char line[100];
+	FILE *fptr;
 	
+	snprintf(filepath, 256, "/proc/%ld/stat", pid);
+	fptr = fopen(filepath, "r");
+
+	if(!fptr){	
+		return;
+	}	
+
+	while(fgets(line, 100, fptr)){
+	
+	}
 
 }
 
-static void readStatm(long int pids, struct pidStats *pid){
+static void readStatm(long int pid, struct pidStats *pidStat){
+	char filepath[256];
+	char line[100];
+	FILE *fptr;
+	
+	snprintf(filepath, 256, "/proc/%ld/statm", pid);
+	fptr = fopen(filepath, "r");
+
+	if(!fptr){	
+		return;
+	}
+
+	while(fgets(line, 100, fptr)){
+	
+	}
+}
+
+static void readCmdline(long int pid, struct pidStats *pidStat){
+	char filepath[256];
+	char line[256];
+	FILE *fptr;
+	
+	snprintf(filepath, 256, "/proc/%ld/cmdline", pid);
+	fptr = fopen(filepath, "r");
+
+	if(!fptr){	
+		return;
+	}
+
+	while(fgets(line, 256, fptr)){
+	
+	}
 
 }
+
 
 int main(void){
 /*
