91e716f4de52e8466030f1f3c83ee98723bf4bfb
Mick K <salabon77mk@gmail.com>
2018-09-17 18:20:18 -0500

    Only prints fields right now, but will change later to modify struct fields

--- statexplore.c
+++ statexplore.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include "procexplore.h"
 
 struct pidStats {
@@ -18,24 +19,25 @@ struct pidStats {
 	char *cmdline;
 };
 
+static void readStatm(long int pid, struct pidStats *pidStat);
 static void readCmdline(long int pid, struct pidStats *pidStat);
 static void readStat(long int pid, struct pidStats *pidStat);
-static void readStatm(long int pid, struct pidStats *pidStat);
 
-void getpidinfo(long int *pids, struct pidStats **stats, size_t pidSize, size_t statStructCount){
+void getpidinfo(long int *pids, struct pidStats **stats, size_t statStructCount){
 	for(int i = 0; i < statStructCount / sizeof(stats) ; i++){
-		readStat(pids[i], stats[i]);
+//		readStat(pids[i], stats[i]);
 //		readStatm(pids[i], stats[i]);
-//		readCmdline(pids[i], stats[i]);
+		readCmdline(pids[i], stats[i]);
 	}
 
 
 
 }
 
+/*
 static void readStat(long int pid, struct pidStats *pidStat){
 	char filepath[256];
-	char line[100];
+	char line[2048];
 	FILE *fptr;
 	
 	snprintf(filepath, 256, "/proc/%ld/stat", pid);
@@ -44,13 +46,37 @@ static void readStat(long int pid, struct pidStats *pidStat){
 	if(!fptr){	
 		return;
 	}	
+	
+	
+	char *lineptr = line;
+	char token[2] = " ";
+	fgets(line, 2048, fptr);
+//	printf("LINE: %s \n NUM: %d \n", line, count);
 
-	while(fgets(line, 100, fptr)){
-		
+	int count = 0;
+	char* holdRes = strtok(lineptr, token);  
+	while(holdRes != NULL){
+	
+	if(count == 2){
+		printf("STATE: %s\n", holdRes); 
+	}
+
+	if(count == 13){
+		printf("UTIME: %s\n", holdRes);
+	}
+
+	if(count ==14){
+		printf("STIME: %s\n", holdRes);
+	}
+
+	holdRes = strtok(NULL, token);
+	count++;
 	}
 
 }
-/**
+
+
+
 static void readStatm(long int pid, struct pidStats *pidStat){
 	char filepath[256];
 	char line[2048];
@@ -61,17 +87,13 @@ static void readStatm(long int pid, struct pidStats *pidStat){
 
 	if(!fptr){	
 		return;
-	}
-
+	}	
 	
-
 	
-	while(fgets(line, 2048, fptr)){
-		char *lineConts = line;
-		sscanf(lineCo
-	}
 	
 }
+*/
+
 
 static void readCmdline(long int pid, struct pidStats *pidStat){
 	char filepath[256];
@@ -85,13 +107,25 @@ static void readCmdline(long int pid, struct pidStats *pidStat){
 		return;
 	}
 
-	while(fgets(line, 256, fptr)){
-		//Test this one out, not sure of behavior
-		pidStat->cmdline = strtok(line, "\0");		
+
+	char *lineptr = line;
+	char token[2] = "\0";
+	fgets(line, 2048, fptr);
+//	printf("LINE: %s \n NUM: %d \n", line, count);
+
+	int count = 0;
+	char* holdRes = strtok(lineptr, token);  
+	
+	printf("%zu\n", pid);
+	while(holdRes != NULL){
+	printf("%s\n", holdRes);	
+	holdRes = strtok(NULL, token);
+	count++;
 	}
+	printf("\n");
 
 }
-*/
+
 
 int main(void){
 
@@ -107,6 +141,7 @@ int main(void){
 	for(int i = 0; i < arrSize; i++){
 		pidArr[i] =  malloc(sizeof(struct pidStats));
 	}
+	getpidinfo(ptrArr, pidArr, arrSize);
 
 	/*	
 	for(int i = 0; i < sizeof(someArray)/sizeof(long int); i++){
